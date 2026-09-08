82c884d3442378579631d05496e5f887b3c3cb0c
Free Coding <freecodingmalayalam2025@gmail.com>
2026-09-08 18:32:00 +0530

    Save and Load Image using entity
    Save and Load Image using entity

---
+++ Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace WindowsFormsApp_Load_Save_image
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnbrowse = new System.Windows.Forms.Button();
+            this.lblfilename = new System.Windows.Forms.Label();
+            this.btnupload = new System.Windows.Forms.Button();
+            this.dgv = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(57, 65);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(264, 164);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnbrowse
+            // 
+            this.btnbrowse.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.btnbrowse.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnbrowse.Location = new System.Drawing.Point(57, 24);
+            this.btnbrowse.Name = "btnbrowse";
+            this.btnbrowse.Size = new System.Drawing.Size(106, 35);
+            this.btnbrowse.TabIndex = 1;
+            this.btnbrowse.Text = "Browse";
+            this.btnbrowse.UseVisualStyleBackColor = false;
+            this.btnbrowse.Click += new System.EventHandler(this.btnbrowse_Click);
+            // 
+            // lblfilename
+            // 
+            this.lblfilename.AutoSize = true;
+            this.lblfilename.Font = new System.Drawing.Font("Courier New", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblfilename.Location = new System.Drawing.Point(195, 32);
+            this.lblfilename.Name = "lblfilename";
+            this.lblfilename.Size = new System.Drawing.Size(0, 20);
+            this.lblfilename.TabIndex = 2;
+            // 
+            // btnupload
+            // 
+            this.btnupload.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.btnupload.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnupload.Location = new System.Drawing.Point(57, 235);
+            this.btnupload.Name = "btnupload";
+            this.btnupload.Size = new System.Drawing.Size(106, 35);
+            this.btnupload.TabIndex = 3;
+            this.btnupload.Text = "Upload";
+            this.btnupload.UseVisualStyleBackColor = false;
+            this.btnupload.Click += new System.EventHandler(this.btnupload_Click);
+            // 
+            // dgv
+            // 
+            this.dgv.AllowUserToResizeRows = false;
+            this.dgv.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv.Location = new System.Drawing.Point(57, 301);
+            this.dgv.MultiSelect = false;
+            this.dgv.Name = "dgv";
+            this.dgv.ReadOnly = true;
+            this.dgv.RowHeadersWidth = 51;
+            this.dgv.RowTemplate.Height = 24;
+            this.dgv.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgv.Size = new System.Drawing.Size(700, 150);
+            this.dgv.TabIndex = 4;
+            this.dgv.DoubleClick += new System.EventHandler(this.dgv_DoubleClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(800, 474);
+            this.Controls.Add(this.dgv);
+            this.Controls.Add(this.btnupload);
+            this.Controls.Add(this.lblfilename);
+            this.Controls.Add(this.btnbrowse);
+            this.Controls.Add(this.pictureBox1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Save & Load Image";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnbrowse;
+        private System.Windows.Forms.Label lblfilename;
+        private System.Windows.Forms.Button btnupload;
+        private System.Windows.Forms.DataGridView dgv;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp_Load_Save_image
+{
+    public partial class Form1 : Form
+    {
+        string FileName;
+        FreeCodingDBEntities db=new FreeCodingDBEntities();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnbrowse_Click(object sender, EventArgs e)
+        {
+            using(OpenFileDialog ofd = new OpenFileDialog() { Multiselect=false,Filter="jpeg|*.jpg"})
+            {
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    FileName = ofd.FileName;
+                    lblfilename.Text = FileName;
+                    pictureBox1.Image = Image.FromFile(FileName);
+                }
+            }
+        }
+
+        byte[] ConvertImageToBinary(Image img)
+        {
+            using (MemoryStream ms = new MemoryStream())
+            {
+                img.Save(ms, System.Drawing.Imaging.ImageFormat.Jpeg);
+                return ms.ToArray();
+            }
+        }
+        private void btnupload_Click(object sender, EventArgs e)
+        {
+            using(db=new FreeCodingDBEntities())
+            {
+                FileData _filedata=new FileData ();
+                _filedata.FileName = FileName;
+                _filedata.Photo = ConvertImageToBinary(pictureBox1.Image);
+                db.FileDatas.Add(_filedata);
+                db.SaveChanges();
+                MessageBox.Show("Photo uploaded successfully");
+                Clear();
+            }
+        }
+
+        void LoadData()
+        {
+            using (db = new FreeCodingDBEntities())
+            {
+                var filelist=db.FileDatas.ToList(); 
+                
+                dgv.DataSource = filelist;
+
+                DataGridViewImageColumn imgCol =
+                 (DataGridViewImageColumn)dgv.Columns["Photo"];
+
+                imgCol.ImageLayout = DataGridViewImageCellLayout.Zoom;
+
+
+                dgv.RowTemplate.Height = 80;
+
+
+                imgCol.Width = 100;
+
+            }
+        }
+        void Clear()
+        {
+           // pictureBox1.Image.Dispose();
+            pictureBox1.Image = null;
+            lblfilename.Text=string.Empty;
+            LoadData();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Clear();
+        }
+
+        private void dgv_DoubleClick(object sender, EventArgs e)
+        {
+            if (dgv.CurrentRow.Index >= 0)
+            {
+                byte[] _imgdata = (byte[]) dgv.CurrentRow.Cells["Photo"].Value;
+                pictureBox1.Image = ConvertBinarytoImage(_imgdata);
+            }
+        }
+
+
+        Image ConvertBinarytoImage(byte[] data)
+        {
+            using (MemoryStream ms = new MemoryStream(data))
+            {
+                return Image.FromStream(ms);
+            }
+        }
+
+    }
+}
